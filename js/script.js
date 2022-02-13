dcd3e231521687887e3d0320741740f15e45f64e
makerofdreams <himanshupapola.ph@gmail.com>
2022-02-14 01:35:26 +0530

    New

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+//calling function after every 1 second 
+//setInterval(mySeconds, 1000);
+
+
+function mySeconds() {
+    const d = new Date();
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let date = document.querySelector('#date');
+    date.innerHTML = d.getDate();
+
+    const month = document.querySelector('#month');
+    month.innerHTML = d.getMonth() + 1;
+
+    let year = document.querySelector('#year');
+    year.innerHTML = d.getFullYear().toString().slice(2, 4);
+
+    let hour = document.querySelector('#hour');
+    hour.innerHTML = d.getHours();
+
+    let minutes = document.querySelector('#minutes');
+    let trueMinutes = d.getMinutes();
+    if (trueMinutes < 10) {
+        minutes.innerHTML = '0' + trueMinutes.toString();
+    } else {
+        minutes.innerHTML = trueMinutes;
+    }
+
+    let seconds = document.querySelector('#seconds');
+    seconds.innerHTML = d.getSeconds();
+
+    let day = document.querySelector('#day');
+    day.innerHTML = days[d.getDay()]
+
+}
+
+
+
---
+++ js/weather.js
@@ -0,0 +1,29 @@
+
+const api = {
+    key: "fcc8de7015bbb202209bbf0261babf4c",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchbox.value);
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        }).then(displayResults);
+}
+
+// invalid location will not be handled 
+function displayResults(weather) {
+document.querySelector('.search-box').style.display = 'none';
+    let temp = document.querySelector('#temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}`;
+}
+
